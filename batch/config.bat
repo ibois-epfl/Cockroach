2cdd337bbf0b7200b5411b287b2ea9cd177edc22
Andrea Settimi <andreasettimi39@gmail.com>
2022-01-05 11:47:57 +0100

    not working by integrating open3d, commented thirdparties cmakelists file out

--- batch/config.bat
+++ batch/config.bat
@@ -4,4 +4,4 @@ ECHO Building Cockroach sln
 ECHO ============================================================
 rmdir build
 mkdir build
-cmake -S . -B build -G "Visual Studio 16 2019" -A x64
+cmake -S . -B build -G "Visual Studio 16 2019" -A x64 -DBUILD_EXAMPLES=OFF -DBUILD_PYTHON_MODULE=OFF -DBUILD_CACHED_CUDA_MANAGER=OFF
