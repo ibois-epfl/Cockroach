d2e1255650db15a1dcdc4cc72575a622235c04ab
Andrea Settimi <andreasettimi39@gmail.com>
2022-01-05 15:30:56 +0100

    modfied example file

--- examples/show_the_bug/src/main.cpp
+++ examples/show_the_bug/src/main.cpp
@@ -1,17 +1,11 @@
 #include <Cockroach.hpp>
 #include <iostream>
 
-
 int main()
 {
+    std::shared_ptr<open3d::geometry:PointCloud> cloud = importCloud("../cloud/cube.ply")
+    Cockroach::visualize_standard(cloud)
 
-    // std::string value = Cockroach::greetings();
-    // std::cout << value << std::endl;
-
-    // return 0;
-
-    
-
-
+    return 0;
 }
 
--- include/Cockroach.hpp
+++ include/Cockroach.hpp
@@ -6,5 +6,5 @@
 // #include "Cockroach/Conversion.hpp"
 // #include "Cockroach/Meshing.hpp"
 // #include "Cockroach/Registration.hpp"
-// #include "Cockroach/Utility.hpp"
+#include "Cockroach/Utility.hpp"
 #include "Cockroach/Visualization.hpp"
