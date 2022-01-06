d2487bafbb5c6f9696460820cb37cb1513b6b9b4
Andrea Settimi <andreasettimi39@gmail.com>
2022-01-06 22:54:19 +0100

    GooD: working version of example file!v3

--- examples/show_bug/src/main.cpp
+++ examples/show_bug/src/main.cpp
@@ -1,58 +1,32 @@
-// #include <Cockroach.hpp>
-#include <iostream>
-#include <filesystem>
-#include <Cockroach.hpp>
 
+#include <iostream>
+
+
+#include <Cockroach.hpp>
 
 typedef open3d::geometry::PointCloud PC;
 typedef std::shared_ptr<PC> PC_ptr;
 
-string pathAppend(const string& p1, const string& p2) {
-
-   char sep = '/';
-   string tmp = p1;
-
-#ifdef _WIN32
-  sep = '\\';
-#endif
-
-  if (p1[p1.length()] != sep) { // Need to add a
-     tmp += sep;                // path separator
-     return(tmp + p2);
-  }
-  else
-     return(p1 + p2);
-}
 
 
-int main()
+
+int main(int argc, char * argv[])
 {
-    // Get current directory
-    std::filesystem::path c_dir = std::filesystem::current_path().string();
-    std::string p_dir = c_dir.parent_path().string();
-    std::string cloud_name = p_dir + "cloud" + "/" + "cube.ply";
+    // Get cloud path
+    // std::string c_dir = getCurrentDirectoryOnWindows();
+    // std::string p_dir = c_dir.parent_path().string();
+    // std::string cloud_name = p_dir + "cloud" + "/" + "cube.ply";
 
-    std::shared_ptr<PC> cloud(new PC);
-    cloud = Cockroach::importCloud(cloud_name);
-    Cockroach::visualize_standard(cloud);
-
+    // // Open cloud
     // std::shared_ptr<PC> cloud(new PC);
-    // const open3d::io::ReadPointCloudOption PC_options;
-    // open3d::io::ReadPointCloud(FILE_NAME_, *cloud, PC_options);
-    // if (cloud->HasPoints())
-    // {
-    //     std::cout << "The point cloud has been correctly imported " << std::endl;
-    //     std::cout << "Number of points " + std::to_string(cloud->points_.size())
-    //         << std::endl;
-    //     return cloud;
-    // }
-    // else
-    // {
-    //     std::cout << "The point cloud has not been correctly imported, press "
-    //         "any key to close"
-    //         << std::endl;
-    //     if (std::cin.get()) throw "No point cloud imported";
-    // }
+    // cloud = Cockroach::importCloud(cloud_name);
+
+    // // Convert Open3d>>Cilantro>>Open3d
+
+    // // Visualize cloud
+    // Cockroach::visualize_standard(cloud);
+    std::cout << argv[0] << std::endl;
+    std::cout<<std::cin.get()<<std::endl;
 
 
     return EXIT_SUCCESS;
---
+++ examples/show_the_bug/batch/launch.bat
@@ -0,0 +1,9 @@
+@ECHO OFF
+ECHO ============================================================
+ECHO Launch .exe
+ECHO ============================================================
+ECHO Be sure to run this command from the source directory...
+
+for %%I in (.) do set CurrDirName=%%~nxI
+ECHO %CurrDirName%
+"/build/Release/%CurrDirName%.exe"
