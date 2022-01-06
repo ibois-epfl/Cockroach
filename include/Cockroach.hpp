d6d0cf4331440ed178c93892c23d6e131e1e15b2
Andrea Settimi <andreasettimi39@gmail.com>
2022-01-06 23:39:30 +0100

    Last version before CIlantro implementation

--- include/Cockroach.hpp
+++ include/Cockroach.hpp
@@ -3,7 +3,7 @@
 
 // // Cockroach headers
 // #include "Cockroach/Clustering.hpp"
-// #include "Cockroach/Conversion.hpp"
+#include "Cockroach/Conversion.hpp"
 // #include "Cockroach/Meshing.hpp"
 // #include "Cockroach/Registration.hpp"
 #include "Cockroach/Utility.hpp"
