e82852cb451842735657b2c0a273bde1e95704f1
Andrea Settimi <andreasettimi39@gmail.com>
2022-01-07 02:44:11 +0100

    cleaned out cmakelist file project example

--- include/Cockroach.hpp
+++ include/Cockroach.hpp
@@ -1,10 +1,7 @@
-// Test
-#include "Cockroach/greetings.hpp"
-
-// // Cockroach headers
-// #include "Cockroach/Clustering.hpp"
+// Interface header
+#include "Cockroach/Clustering.hpp"
 #include "Cockroach/Conversion.hpp"
-// #include "Cockroach/Meshing.hpp"
-// #include "Cockroach/Registration.hpp"
+#include "Cockroach/Meshing.hpp"
+#include "Cockroach/Registration.hpp"
 #include "Cockroach/Utility.hpp"
 #include "Cockroach/Visualization.hpp"
