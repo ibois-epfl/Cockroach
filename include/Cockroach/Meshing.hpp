5f17569770e6322b57c2e2fae5691b15d1a0d5f7
Andrea Settimi <andreasettimi39@gmail.com>
2022-01-07 15:20:35 +0100

    First commit with Cockroach v. 1.0.1 header only library. Only vanilla flavour for now

--- include/Cockroach/Meshing.hpp
+++ include/Cockroach/Meshing.hpp
@@ -1,24 +1,24 @@
-// #pragma once
+#pragma once
 
-// #include <iostream>
+#include <iostream>
 
-// // Open3D
-// #include "open3d/Open3D.h"
-// #include <open3d/pipelines/registration/RobustKernel.h>
-// #include <open3d/pipelines/registration/ColoredICP.h>
-// #include <open3d/pipelines/registration/FastGlobalRegistration.h>
+// Open3D
+#include "open3d/Open3D.h"
+#include <open3d/pipelines/registration/RobustKernel.h>
+#include <open3d/pipelines/registration/ColoredICP.h>
+#include <open3d/pipelines/registration/FastGlobalRegistration.h>
 
-// typedef open3d::geometry::PointCloud PC;
-// typedef std::shared_ptr<PC> PC_ptr;
+typedef open3d::geometry::PointCloud PC;
+typedef std::shared_ptr<PC> PC_ptr;
 
 
-// namespace Cockroach 
-// {
-//     /*
-//     Cockroach does not implement custom meshing techniques but instead uses the ones provided
-//     by Open3D library, mainly Poission and Ball-pivoting reconstruction.
+namespace Cockroach 
+{
+    /*
+    Cockroach does not implement custom meshing techniques but instead uses the ones provided
+    by Open3D library, mainly Poission and Ball-pivoting reconstruction.
 
-//     Source:
-//     http://www.open3d.org/docs/latest/tutorial/Advanced/surface_reconstruction.html
-//     */
-// }
+    Source:
+    http://www.open3d.org/docs/latest/tutorial/Advanced/surface_reconstruction.html
+    */
+}
